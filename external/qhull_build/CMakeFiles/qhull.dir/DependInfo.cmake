0dfc8ac29b684ca4cd1edcc4fce7d4eda017d4a2
formaggia <luca.formaggia@gmail.com>
2013-03-07 15:42:13 +0100

    Directory to build qhull. You need to rerun cmake-gui to fix CMAKE_PREFIX which should point to the external/ directory

---
+++ external/qhull_build/CMakeFiles/qhull.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/forma/workspace2/eni/eniReservoir/external/qhull-2012.1/src/qhull/unix.c" "/home/forma/workspace2/eni/eniReservoir/external/qhull_build/CMakeFiles/qhull.dir/src/qhull/unix.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/forma/workspace2/eni/eniReservoir/external/qhull_build/CMakeFiles/qhullstatic.dir/DependInfo.cmake"
+  )
---
+++ external/qhull_build/CMakeFiles/user_eg.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/forma/workspace2/eni/eniReservoir/external/qhull-2012.1/src/user_eg/user_eg.c" "/home/forma/workspace2/eni/eniReservoir/external/qhull_build/CMakeFiles/user_eg.dir/src/user_eg/user_eg.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "qh_QHpointer"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/forma/workspace2/eni/eniReservoir/external/qhull_build/CMakeFiles/qhull_p.dir/DependInfo.cmake"
+  )
