db4c8020c1fccf18cd8083e29fe9f04010206a5a
formaggia <luca.formaggia@gmail.com>
2013-07-14 15:42:10 +0200

    No more eps in TypeDefinitions
    TypeDefinitions is only for types. Tolerances go
    in tolerances.hpp

--- EDFM/src/TypeDefinition.hpp
+++ EDFM/src/TypeDefinition.hpp
@@ -8,12 +8,8 @@
 #ifndef TYPEDEFNITION_HPP_
 #define TYPEDEFNITION_HPP_
 
-
 //! Type for real numbers
 typedef double Real;
-
-//! Defining the zero limit
-const Real eps = 1e-10;
 
 //! Type for real numbers
 typedef unsigned int UInt;
