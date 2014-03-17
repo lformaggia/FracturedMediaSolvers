aceb0165043187475cd0669684c1805c7e6a2391
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2014-03-17 15:40:28 +0100

    Fix the inclusions in a file.

--- FVCode3D/tools/converterToVTU/converterToVTU.cpp
+++ FVCode3D/tools/converterToVTU/converterToVTU.cpp
@@ -10,53 +10,54 @@
 #include "mesh/Mesh3D.hpp"
 #include "mesh/Properties.hpp"
 #include "import/import.hpp"
-#include "export/export.hpp"
+#include "export/exportVTK.hpp"
+#include "export/exportVTU.hpp"
 
 int main(int argc, char * argv[])
 {
-	GetPot command_line(argc,argv);
-	const std::string dataFileName = command_line.follow("data.txt", 2, "-f", "--file");
+    GetPot command_line(argc,argv);
+    const std::string dataFileName = command_line.follow("data.txt", 2, "-f", "--file");
 
-	std::cout << "Read Data..." << std::flush;
-	Data data(dataFileName);
-	std::cout << " done." << std::endl;
+    std::cout << "Read Data..." << std::flush;
+    Data data(dataFileName);
+    std::cout << " done." << std::endl;
 
-	std::cout << std::endl;
-	data.showMe();
-	std::cout << std::endl;
+    std::cout << std::endl;
+    data.showMe();
+    std::cout << std::endl;
 
-	std::cout << "Define Mesh and Properties..." << std::flush;
-	Geometry::Mesh3D mesh;
-	Geometry::PropertiesMap propMap(data.getMobility());
-	std::cout << " done." << std::endl;
+    std::cout << "Define Mesh and Properties..." << std::flush;
+    Geometry::Mesh3D mesh;
+    Geometry::PropertiesMap propMap(data.getMobility());
+    std::cout << " done." << std::endl;
 
-	std::cout << "Create Importer..." << std::flush;
-	ImporterForSolver importer(data.getMeshDir() + data.getMeshFile(), mesh, propMap);
-	std::cout << " done." << std::endl;
+    std::cout << "Create Importer..." << std::flush;
+    ImporterForSolver importer(data.getMeshDir() + data.getMeshFile(), mesh, propMap);
+    std::cout << " done." << std::endl;
 
-	std::cout << "Import grid file..." << std::flush;
-	importer.import(data.fractureOn());
-	std::cout << " done." << std::endl << std::endl;
+    std::cout << "Import grid file..." << std::flush;
+    importer.import(data.fractureOn());
+    std::cout << " done." << std::endl << std::endl;
 
-	std::cout << "Compute separated cells..." << std::flush;
-	mesh.updateFacetsWithCells();
-	std::cout << " done." << std::endl;
+    std::cout << "Compute separated cells..." << std::flush;
+    mesh.updateFacetsWithCells();
+    std::cout << " done." << std::endl;
 
-	std::cout << "Compute neighboring cells..." << std::flush;
-	mesh.updateCellsWithNeighbors();
-	std::cout << " done." << std::endl << std::endl;
+    std::cout << "Compute neighboring cells..." << std::flush;
+    mesh.updateCellsWithNeighbors();
+    std::cout << " done." << std::endl << std::endl;
 
-	std::cout << "Compute facet ids of the fractures..." << std::flush;
-	mesh.updateFacetsWithFractures();
-	std::cout << " done." << std::endl << std::endl;
+    std::cout << "Compute facet ids of the fractures..." << std::flush;
+    mesh.updateFacetsWithFractures();
+    std::cout << " done." << std::endl << std::endl;
 
-	std::cout << "Export..." << std::flush;
-	ExporterVTU exporter;
-	exporter.exportMesh(mesh, data.getOutputDir() + data.getOutputFile() + "_mesh.vtu");
-	exporter.exportFractures(mesh, data.getOutputDir() + data.getOutputFile() + "_fractures.vtu");
-	exporter.exportMeshWithFractures(mesh, data.getOutputDir() + data.getOutputFile() + "_mesh_fracture.vtu");
-	exporter.exportWithProperties(mesh, propMap, data.getOutputDir() + data.getOutputFile() + "_prop.vtu");
-	std::cout << " done." << std::endl << std::endl;
+    std::cout << "Export..." << std::flush;
+    ExporterVTU exporter;
+    exporter.exportMesh(mesh, data.getOutputDir() + data.getOutputFile() + "_mesh.vtu");
+    exporter.exportFractures(mesh, data.getOutputDir() + data.getOutputFile() + "_fractures.vtu");
+    exporter.exportMeshWithFractures(mesh, data.getOutputDir() + data.getOutputFile() + "_mesh_fracture.vtu");
+    exporter.exportWithProperties(mesh, propMap, data.getOutputDir() + data.getOutputFile() + "_prop.vtu");
+    std::cout << " done." << std::endl << std::endl;
 
-	return 0;
+    return 0;
 }
