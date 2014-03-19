1b12d07a2f9ae009a2a7a1773ca7df5524746ace
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2014-03-19 10:54:32 +0100

    Add the LU factorization

--- FVCode3D/src/solver/solver.cpp
+++ FVCode3D/src/solver/solver.cpp
@@ -1,12 +1,20 @@
 /*!
- *	@file solver.cpp
- *	@brief These classes allow to solve a linear system (definitions).
+ *  @file solver.cpp
+ *  @brief These classes allow to solve a linear system (definitions).
  */
 
 #include "solver/solver.hpp"
 
 void EigenCholesky::solve()
 {
-	Eigen::SimplicialCholesky<SpMat> chol(M_A);
-	M_x = chol.solve(M_b);
+    Eigen::SimplicialCholesky<SpMat> chol(M_A);
+    M_x = chol.solve(M_b);
 }
+
+void EigenLU::solve()
+{
+    Eigen::SparseLU<SpMat> lu;
+    lu.analyzePattern(M_A);
+    lu.factorize(M_A);
+    M_x = lu.solve(M_b);
+} // solve
--- FVCode3D/src/solver/solver.hpp
+++ FVCode3D/src/solver/solver.hpp
@@ -1,6 +1,6 @@
 /*!
- *	@file solver.hpp
- *	@brief These classes allow to solve a linear system.
+ *  @file solver.hpp
+ *  @brief These classes allow to solve a linear system.
  */
 
 #ifndef SOLVER_HPP_
@@ -19,68 +19,68 @@ class Solver
 {
 public:
 
-	//! Constructor
-	/*!
-	 * @param A Eigen sparse matrix
-	 * @param b RHS, it is Eigen vector
-	 */
-	Solver(const SpMat & A, const Vector & b):
-		M_A(A), M_b(b) {}
+    //! Constructor
+    /*!
+     * @param A Eigen sparse matrix
+     * @param b RHS, it is Eigen vector
+     */
+    Solver(const SpMat & A, const Vector & b):
+        M_A(A), M_b(b) {}
 
-	//! Get the matrix A
-	/*!
-	 * @return the matrix A
-	 */
-	const SpMat & getA() const
-		{ return M_A; }
+    //! Get the matrix A
+    /*!
+     * @return the matrix A
+     */
+    const SpMat & getA() const
+        { return M_A; }
 
-	//! Get the RHS
-	/*!
-	 * @return the vector b
-	 */
-	const Vector & getb() const
-		{ return M_b; }
+    //! Get the RHS
+    /*!
+     * @return the vector b
+     */
+    const Vector & getb() const
+        { return M_b; }
 
-	//! Get the solution
-	/*!
-	 * @return the vector containing the solution
-	 * @pre call solve()
-	 */
-	const Vector & getSolution() const
-		{ return M_x; }
+    //! Get the solution
+    /*!
+     * @return the vector containing the solution
+     * @pre call solve()
+     */
+    const Vector & getSolution() const
+        { return M_x; }
 
-	//! Get the system size
-	/*!
-	 * @return the system size, i.e. the number of rows of A
-	 */
-	UInt size() const
-		{ return M_A.rows(); }
+    //! Get the system size
+    /*!
+     * @return the system size, i.e. the number of rows of A
+     */
+    UInt size() const
+        { return M_A.rows(); }
 
-	//! Get the non zero values
-	/*!
-	 * @return the number or non zero values
-	 */
-	UInt nonZeros() const
-		{ return M_A.nonZeros(); }
+    //! Get the non zero values
+    /*!
+     * @return the number or non zero values
+     */
+    UInt nonZeros() const
+        { return M_A.nonZeros(); }
 
-	//! Solve the linear system
-	/*!
-	 * Solve the linear system Ax=b.
-	 * The method employed to solve the system depends on the derived class of Solver that calls this method.
-	 */
-	virtual void solve() = 0;
+    //! Solve the linear system
+    /*!
+     * Solve the linear system Ax=b.
+     * The method employed to solve the system depends on the derived class of Solver that calls this method.
+     */
+    virtual void solve() = 0;
 
-	//! Destructor
-	virtual ~Solver() {}
+    //! Destructor
+    virtual ~Solver() {}
 
 protected:
 
-	//! Sparse Matrix
-	const SpMat & M_A;
-	//! RHS
-	const Vector & M_b;
-	//! Solution vector
-	Vector M_x;
+    //! Sparse Matrix
+    const SpMat & M_A;
+    //! RHS
+    const Vector & M_b;
+    //! Solution vector
+    Vector M_x;
 
 };
 
@@ -88,22 +88,45 @@ class EigenCholesky : public Solver
 {
 public:
 
-	//! Constructor
-	/*!
-	 * @param A Eigen sparse matrix
-	 * @param b RHS, it is Eigen vector
-	 */
-	EigenCholesky(const SpMat & A, const Vector & b):
-		Solver(A,b) {}
+    //! Constructor
+    /*!
+     * @param A Eigen sparse matrix
+     * @param b RHS, it is Eigen vector
+     */
+    EigenCholesky(const SpMat & A, const Vector & b):
+        Solver(A,b) {}
 
-	//! Solve the linear system
-	/*!
-	 * Solve the linear system Ax=b by means of a Cholesky factorization.
-	 */
-	virtual void solve();
+    //! Solve the linear system
+    /*!
+     * Solve the linear system Ax=b by means of a Cholesky factorization.
+     */
+    virtual void solve();
 
-	//! Destructor
-	virtual ~EigenCholesky() {}
+    //! Destructor
+    virtual ~EigenCholesky() {}
+
+};
+
+class EigenLU : public Solver
+{
+public:
+
+    //! Constructor
+    /*!
+     * @param A Eigen sparse matrix
+     * @param b RHS, it is Eigen vector
+     */
+    EigenLU(const SpMat & A, const Vector & b):
+        Solver(A,b) {}
+
+    //! Solve the linear system
+    /*!
+     * Solve the linear system Ax=b by means of a Cholesky factorization.
+     */
+    virtual void solve();
+
+    //! Destructor
+    virtual ~EigenLU() {}
 
 };
 
