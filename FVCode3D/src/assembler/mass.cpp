c27aa5f886df1c4ac213115cb47d7ea7c5817ba6
Stefano Zonca <zstefano123@gmail.com>
2014-03-20 12:05:54 +0100

    Added the compressibility parameter to the mass matrix assembler

--- FVCode3D/src/assembler/mass.cpp
+++ FVCode3D/src/assembler/mass.cpp
@@ -13,11 +13,12 @@ void MassMatrix::assemble()
 {
 	std::vector<Triplet> Matrix_elements;
 	Real porosity, volume;
+	const Real compressibility = M_properties.getCompressibility();
 
 	for (auto cell_it : this->M_mesh.getCellsVector())
 	{
 		porosity = M_properties.getProperties(cell_it.getZoneCode()).M_porosity;
-		Matrix_elements.emplace_back(Triplet (cell_it.getId(), cell_it.getId(), porosity * cell_it.getVolume()));
+		Matrix_elements.emplace_back(Triplet (cell_it.getId(), cell_it.getId(), compressibility * porosity * cell_it.getVolume()));
 	}
 
 	for (auto facet_it : this->M_mesh.getFractureFacetsIdsVector())
@@ -26,15 +27,15 @@ void MassMatrix::assemble()
 		volume = M_properties.getProperties(facet_it.getZoneCode()).M_aperture * facet_it.getFacet().area();
 		porosity = M_properties.getProperties(facet_it.getZoneCode()).M_porosity;
 
-		Matrix_elements.emplace_back(Triplet (facet_it.getIdasCell(), facet_it.getIdasCell(), porosity * volume));
+		Matrix_elements.emplace_back(Triplet (facet_it.getIdasCell(), facet_it.getIdasCell(), compressibility * porosity * volume));
 
 		// remove from the first neighbor the portion due to the fracture
 		porosity = M_properties.getProperties( this->M_mesh.getCellsVector()[facet_it.getSeparated()[0]].getZoneCode() ).M_porosity;
-		Matrix_elements.emplace_back(Triplet (facet_it.getSeparated()[0], facet_it.getSeparated()[0], -porosity * volume / 2));
+		Matrix_elements.emplace_back(Triplet (facet_it.getSeparated()[0], facet_it.getSeparated()[0], -compressibility * porosity * volume / 2));
 
 		// remove from the second neighbor the portion due to the fracture
 		porosity = M_properties.getProperties( this->M_mesh.getCellsVector()[facet_it.getSeparated()[1]].getZoneCode() ).M_porosity;
-		Matrix_elements.emplace_back(Triplet (facet_it.getSeparated()[1], facet_it.getSeparated()[1], -porosity * volume / 2));
+		Matrix_elements.emplace_back(Triplet (facet_it.getSeparated()[1], facet_it.getSeparated()[1], -compressibility * porosity * volume / 2));
 	}
 	this->M_Matrix->setFromTriplets(Matrix_elements.begin(), Matrix_elements.end());
 }
