42ddf2d1aea26e71a739c2129677abdbaf9408a2
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2014-05-28 14:38:08 +0200

    Split the stiffness assembler in several parts, one for the domain, one for the fractures, one for the coupling and the last one for the boundary conditions
    Conflicts:
    
    	FVCode3D/src/assembler/stiffness.cpp
    	FVCode3D/src/assembler/stiffness.hpp

--- FVCode3D/src/assembler/stiffness.hpp
+++ FVCode3D/src/assembler/stiffness.hpp
@@ -1,6 +1,6 @@
 /*!
- *	@file stiffness.hpp
- *	@brief This class build a Stiffness-matrix of the Darcy problem.
+ *  @file stiffness.hpp
+ *  @brief This class build a Stiffness-matrix of the Darcy problem.
  */
 
 #ifndef __DARCYSTIFFNESS_HPP__
@@ -21,65 +21,65 @@ namespace Darcy
 
 //! Class for assembling a stiffness matrix
 /*!
-	@class StiffMatrix
-	This class constructs the stiffness-matrix for the Darcy problem.
-	The adopted technique is a two point finite volume method.
-	The fractures are considered as cells and take part to discretization.
+    @class StiffMatrix
+    This class constructs the stiffness-matrix for the Darcy problem.
+    The adopted technique is a two point finite volume method.
+    The fractures are considered as cells and take part to discretization.
 */
 class StiffMatrix: public MatrixHandler
 {
 
-	//! Typedef for std::pair<UInt,UInt>
-	/*!
-		@typedef Fracture_Juncture
-   		This type definition permits to treat std::pair<UInt,UInt> as a Fracture_Juncture.
-   	*/
-	typedef std::pair<UInt,UInt> Fracture_Juncture;
-	//! Typedef for Geometry::Rigid_Mesh::Facet_ID
-	/*!
-		@typedef Facet_ID
-   		This type definition permits to treat Geometry::Rigid_Mesh::Facet_ID as a Facet_ID.
-   	*/
-	typedef Geometry::Rigid_Mesh::Facet_ID Facet_ID;
+    //! Typedef for std::pair<UInt,UInt>
+    /*!
+        @typedef Fracture_Juncture
+        This type definition permits to treat std::pair<UInt,UInt> as a Fracture_Juncture.
+    */
+    typedef std::pair<UInt,UInt> Fracture_Juncture;
+    //! Typedef for Geometry::Rigid_Mesh::Facet_ID
+    /*!
+        @typedef Facet_ID
+        This type definition permits to treat Geometry::Rigid_Mesh::Facet_ID as a Facet_ID.
+    */
+    typedef Geometry::Rigid_Mesh::Facet_ID Facet_ID;
 
 public:
-	//! @name Constructor & Destructor
-	//@{
+    //! @name Constructor & Destructor
+    //@{
 
-	//! Construct a stiffness-Matrix, given a Geometry::Rigid_Mesh and the boundary conditions
-	/*!
-		@param rigid_mesh A Geometry::Rigid_Mesh used to build the matrix
-		@param BC Boundary conditions given in the container Darcy::BoundaryConditions
-	*/
-	StiffMatrix(const Geometry::Rigid_Mesh & rigid_mesh, const BoundaryConditions & Bc):
-		MatrixHandler(rigid_mesh), _b (new Vector(this->M_size)),
-		M_properties(rigid_mesh.getPropertiesMap()), m_Bc(Bc) {}
-	//! No Copy-Constructor
-	StiffMatrix(const StiffMatrix &) = delete;
-	//! No Empty-Constructor
-	StiffMatrix() = delete;
-	//! Destructor
-	~StiffMatrix() = default;
-	//@}
+    //! Construct a stiffness-Matrix, given a Geometry::Rigid_Mesh and the boundary conditions
+    /*!
+        @param rigid_mesh A Geometry::Rigid_Mesh used to build the matrix
+        @param BC Boundary conditions given in the container Darcy::BoundaryConditions
+    */
+    StiffMatrix(const Geometry::Rigid_Mesh & rigid_mesh, const BoundaryConditions & Bc):
+        MatrixHandler(rigid_mesh), _b (new Vector(this->M_size)),
+        M_properties(rigid_mesh.getPropertiesMap()), m_Bc(Bc) {}
+    //! No Copy-Constructor
+    StiffMatrix(const StiffMatrix &) = delete;
+    //! No Empty-Constructor
+    StiffMatrix() = delete;
+    //! Destructor
+    ~StiffMatrix() = default;
+    //@}
 
-	//! @name Get Methods
-	//@{
-	//! Get BC vector (const)
-	/*!
-	 * @return A reference to a constant vector that represents the part of the right hand side due to the boundary conditions.
-	 */
-	const Vector & getBCVector() const
-		{return *_b;}
-	//@}
+    //! @name Get Methods
+    //@{
+    //! Get BC vector (const)
+    /*!
+     * @return A reference to a constant vector that represents the part of the right hand side due to the boundary conditions.
+     */
+    const Vector & getBCVector() const
+        {return *_b;}
+    //@}
 
-	//! @name Methods
-	//@{
-	//! Assemble method
-	/*!
-	 * @return Assemble the Mass matrix
-	 */
-	void assemble();
-	//@}
+    //! @name Methods
+    //@{
+    //! Assemble method
+    /*!
+     * @return Assemble the stiffness matrix
+     */
+    void assemble();
+    //@}
 
 public:
 
@@ -117,14 +117,38 @@ public:
 	 */
 	Real Findfracturesalpha (const std::pair<UInt,UInt> fj, const UInt n_Id) const;
 	//@}
-	
+
+protected:    
+    
+    //! @name Assemble Methods
+    //@{
+    
+    //! Assemble the porous medium block
+    /*!
+     * @return Assemble the porous media block
+     */
+    void assemblePorousMatrix( std::vector<Triplet>& Matrix_elements ) const;
+    
+    //! Assemble the BCs for the porous medium
+    /*!
+     * @return Assemble the BCs for the porous medium
+     */
+    void assemblePorousMatrixBC( std::vector<Triplet>& Matrix_elements ) const;
+    
+    //! Assemble the fractures block
+    /*!
+     * @return Assemble the fractures block
+     */
+    void assembleFracture( std::vector<Triplet>& Matrix_elements ) const;
+    //@}
+    
 protected:
-	//! Unique pointer to the vector that contains the effects of BCs on the RHS
-	std::unique_ptr<Vector> _b;
-	//! A reference to a Geometry::PropertiesMap
-	const Geometry::PropertiesMap & M_properties;
-	//! The container of the BCs
-	const BoundaryConditions & m_Bc;
+    //! Unique pointer to the vector that contains the effects of BCs on the RHS
+    std::unique_ptr<Vector> _b;
+    //! A reference to a Geometry::PropertiesMap
+    const Geometry::PropertiesMap & M_properties;
+    //! The container of the BCs
+    const BoundaryConditions & m_Bc;
 };
 
 } // namespace Darcy
