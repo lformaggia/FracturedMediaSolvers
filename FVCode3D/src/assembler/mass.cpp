fc4a40b28123c56f8d6104324ee0696aa9d07e25
Stefano Zonca <zstefano123@gmail.com>
2014-06-12 11:09:02 +0200

    MassMatrix: reserved vector of Triplets and fixed emplace_back.

--- FVCode3D/src/assembler/mass.cpp
+++ FVCode3D/src/assembler/mass.cpp
@@ -12,13 +12,15 @@ namespace Darcy
 void MassMatrix::assemble()
 {
 	std::vector<Triplet> Matrix_elements;
+	Matrix_elements.reserve(this->M_mesh.getCellsVector().size() + 3 * this->M_mesh.getFractureFacetsIdsVector().size());
+
 	Real porosity, volume;
 	const Real compressibility = M_properties.getCompressibility();
 
 	for (auto cell_it : this->M_mesh.getCellsVector())
 	{
 		porosity = M_properties.getProperties(cell_it.getZoneCode()).M_porosity;
-		Matrix_elements.emplace_back(Triplet (cell_it.getId(), cell_it.getId(), compressibility * porosity * cell_it.getVolume()));
+		Matrix_elements.emplace_back(cell_it.getId(), cell_it.getId(), compressibility * porosity * cell_it.getVolume()); // Triplet
 	}
 
 	for (auto facet_it : this->M_mesh.getFractureFacetsIdsVector())
@@ -27,15 +29,15 @@ void MassMatrix::assemble()
 		volume = M_properties.getProperties(facet_it.getZoneCode()).M_aperture * facet_it.getFacet().area();
 		porosity = M_properties.getProperties(facet_it.getZoneCode()).M_porosity;
 
-		Matrix_elements.emplace_back(Triplet (facet_it.getIdasCell(), facet_it.getIdasCell(), compressibility * porosity * volume));
+		Matrix_elements.emplace_back(facet_it.getIdasCell(), facet_it.getIdasCell(), compressibility * porosity * volume); // Triplet
 
 		// remove from the first neighbor the portion due to the fracture
 		porosity = M_properties.getProperties( this->M_mesh.getCellsVector()[facet_it.getSeparated()[0]].getZoneCode() ).M_porosity;
-		Matrix_elements.emplace_back(Triplet (facet_it.getSeparated()[0], facet_it.getSeparated()[0], -compressibility * porosity * volume / 2));
+		Matrix_elements.emplace_back(facet_it.getSeparated()[0], facet_it.getSeparated()[0], -compressibility * porosity * volume / 2); // Triplet
 
 		// remove from the second neighbor the portion due to the fracture
 		porosity = M_properties.getProperties( this->M_mesh.getCellsVector()[facet_it.getSeparated()[1]].getZoneCode() ).M_porosity;
-		Matrix_elements.emplace_back(Triplet (facet_it.getSeparated()[1], facet_it.getSeparated()[1], -compressibility * porosity * volume / 2));
+		Matrix_elements.emplace_back(facet_it.getSeparated()[1], facet_it.getSeparated()[1], -compressibility * porosity * volume / 2); // Triplet
 	}
 	this->M_Matrix->setFromTriplets(Matrix_elements.begin(), Matrix_elements.end());
 }
