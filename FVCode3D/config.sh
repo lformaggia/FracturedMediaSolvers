299616343a451109869670a3b661b357fcf16ab3
Stefano Zonca <zstefano123@gmail.com>
2014-06-23 08:23:00 +0200

    Updated example of config.sh

--- FVCode3D/config.sh
+++ FVCode3D/config.sh
@@ -1,1 +1,5 @@
-ccmake ../eniReservoir/FVCode3D
+ccmake ../eniReservoir/FVCode3D \
+    -DTPL_BLAS_DIR=/home/viskius/eniReservoir/FVCode3D-install/external/blas \
+    -DTPL_Eigen_DIR=/home/viskius/eniReservoir/FVCode3D-install/external/eigen \
+    -DTPL_SuiteSparse_DIR=/home/viskius/eniReservoir/FVCode3D-install/external/suitesparse \
+    -DCMAKE_INSTALL_PREFIX=/home/viskius/eniReservoir/FVCode3D-install
