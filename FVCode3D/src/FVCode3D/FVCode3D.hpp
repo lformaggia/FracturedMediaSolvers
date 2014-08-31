5c676ec14f983d110114fe79de655e748c7f5f7e
Stefano Zonca <zstefano123@gmail.com>
2014-08-31 15:03:53 +0200

    Included in FVCode3D.hpp the SolverHandler header

--- FVCode3D/src/FVCode3D/FVCode3D.hpp
+++ FVCode3D/src/FVCode3D/FVCode3D.hpp
@@ -16,6 +16,7 @@
 #include <FVCode3D/core/Data.hpp>
 #include <FVCode3D/core/Chrono.hpp>
 #include <FVCode3D/solver/Solver.hpp>
+#include <FVCode3D/solver/SolverHandler.hpp>
 #include <FVCode3D/import/Import.hpp>
 #include <FVCode3D/quadrature/Quadrature.hpp>
 #include <FVCode3D/quadrature/QuadratureRules.hpp>
