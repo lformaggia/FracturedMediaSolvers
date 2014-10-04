4435022177f208ccb1629b94a9854f672aec9b1e
Stefano Zonca <zstefano123@gmail.com>
2014-10-04 14:30:54 +0200

    Removed config.sh

--- FVCode3D/config.sh
+++
@@ -1,5 +0,0 @@
-ccmake ../eniReservoir/FVCode3D \
-    -DTPL_BLAS_DIR=/home/viskius/eniReservoir/FVCode3D-install/external/blas \
-    -DTPL_Eigen_DIR=/home/viskius/eniReservoir/FVCode3D-install/external/eigen \
-    -DTPL_SuiteSparse_DIR=/home/viskius/eniReservoir/FVCode3D-install/external/suitesparse \
-    -DCMAKE_INSTALL_PREFIX=/home/viskius/eniReservoir/FVCode3D-install
