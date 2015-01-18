88b2e75b31f549e16381772135b077ede3e45fcf
Stefano Zonca <zstefano123@gmail.com>
2015-01-18 15:00:32 +0100

    Removed the volume correction on the porous matrix when computing the Mass matrix and the integrals

--- FVCode3D/src/FVCode3D/assembler/Mass.cpp
+++ FVCode3D/src/FVCode3D/assembler/Mass.cpp
@@ -1,6 +1,6 @@
  /*!
- *	@file mass.cpp
- *	@brief Class for building a Mass-matrix for a finite volume method (definitions).
+ *  @file mass.cpp
+ *  @brief Class for building a Mass-matrix for a finite volume method (definitions).
  */
 
 #include <FVCode3D/assembler/Mass.hpp>
@@ -11,42 +11,42 @@ namespace FVCode3D
 
 void MassMatrix::assemble()
 {
-	M_matrixElements.reserve(this->M_mesh.getCellsVector().size() + 3 * this->M_mesh.getFractureFacetsIdsVector().size());
+    M_matrixElements.reserve(this->M_mesh.getCellsVector().size() + 3 * this->M_mesh.getFractureFacetsIdsVector().size());
 
-	Real porosity, volume;
-	const Real compressibility = M_properties.getCompressibility();
+    Real porosity, volume;
+    const Real compressibility = M_properties.getCompressibility();
 
-	for (auto& cell_it : this->M_mesh.getCellsVector())
-	{
-		porosity = M_properties.getProperties(cell_it.getZoneCode()).M_porosity;
-		M_matrixElements.emplace_back(M_offsetRow + cell_it.getId(), M_offsetCol + cell_it.getId(), compressibility * porosity * cell_it.getVolume()); // Triplet
-	}
+    for (auto& cell_it : this->M_mesh.getCellsVector())
+    {
+        porosity = M_properties.getProperties(cell_it.getZoneCode()).M_porosity;
+        M_matrixElements.emplace_back(M_offsetRow + cell_it.getId(), M_offsetCol + cell_it.getId(), compressibility * porosity * cell_it.getVolume()); // Triplet
+    }
 
-	for (auto& facet_it : this->M_mesh.getFractureFacetsIdsVector())
-	{
-		// set mass term of the fracture
-		volume = M_properties.getProperties(facet_it.getZoneCode()).M_aperture * facet_it.getFacet().area();
-		porosity = M_properties.getProperties(facet_it.getZoneCode()).M_porosity;
+    for (auto& facet_it : this->M_mesh.getFractureFacetsIdsVector())
+    {
+        // set mass term of the fracture
+        volume = M_properties.getProperties(facet_it.getZoneCode()).M_aperture * facet_it.getFacet().area();
+        porosity = M_properties.getProperties(facet_it.getZoneCode()).M_porosity;
 
-		M_matrixElements.emplace_back(M_offsetRow + facet_it.getIdAsCell(), M_offsetCol + facet_it.getIdAsCell(), compressibility * porosity * volume); // Triplet
-	}
+        M_matrixElements.emplace_back(M_offsetRow + facet_it.getIdAsCell(), M_offsetCol + facet_it.getIdAsCell(), compressibility * porosity * volume); // Triplet
+    }
 
-	if( this->M_mesh.getCellsVector().size() > 0 )
-	{
-		for (auto& facet_it : this->M_mesh.getFractureFacetsIdsVector())
-		{
-			// set mass term of the fracture
-			volume = M_properties.getProperties(facet_it.getZoneCode()).M_aperture * facet_it.getFacet().area();
-
-			// remove from the first neighbor the portion due to the fracture
-			porosity = M_properties.getProperties( this->M_mesh.getCellsVector()[facet_it.getSeparatedCellsIds()[0]].getZoneCode() ).M_porosity;
-			M_matrixElements.emplace_back(M_offsetRow + facet_it.getSeparatedCellsIds()[0], M_offsetCol + facet_it.getSeparatedCellsIds()[0], -compressibility * porosity * volume / 2); // Triplet
-
-			// remove from the second neighbor the portion due to the fracture
-			porosity = M_properties.getProperties( this->M_mesh.getCellsVector()[facet_it.getSeparatedCellsIds()[1]].getZoneCode() ).M_porosity;
-			M_matrixElements.emplace_back(M_offsetRow + facet_it.getSeparatedCellsIds()[1], M_offsetCol + facet_it.getSeparatedCellsIds()[1], -compressibility * porosity * volume / 2); // Triplet
-		}
-	}
+//    if( this->M_mesh.getCellsVector().size() > 0 )
+//    {
+//        for (auto& facet_it : this->M_mesh.getFractureFacetsIdsVector())
+//        {
+//            // set mass term of the fracture
+//            volume = M_properties.getProperties(facet_it.getZoneCode()).M_aperture * facet_it.getFacet().area();
+//
+//            // remove from the first neighbor the portion due to the fracture
+//            porosity = M_properties.getProperties( this->M_mesh.getCellsVector()[facet_it.getSeparatedCellsIds()[0]].getZoneCode() ).M_porosity;
+//            M_matrixElements.emplace_back(M_offsetRow + facet_it.getSeparatedCellsIds()[0], M_offsetCol + facet_it.getSeparatedCellsIds()[0], -compressibility * porosity * volume / 2); // Triplet
+//
+//            // remove from the second neighbor the portion due to the fracture
+//            porosity = M_properties.getProperties( this->M_mesh.getCellsVector()[facet_it.getSeparatedCellsIds()[1]].getZoneCode() ).M_porosity;
+//            M_matrixElements.emplace_back(M_offsetRow + facet_it.getSeparatedCellsIds()[1], M_offsetCol + facet_it.getSeparatedCellsIds()[1], -compressibility * porosity * volume / 2); // Triplet
+//        }
+//    }
 }
 
 } // namespace FVCode3D
