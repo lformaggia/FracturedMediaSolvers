20f2c85c05fac655f03bcc8213599f10f50f6544
Luca Pasquale <luca.pasquale@moxoff.com>
2015-03-16 16:49:10 +0100

    Improved CMake files by simplifying handling of third party libraries

--- FVCode3D/cmake/IncludeFunctions.cmake
+++ FVCode3D/cmake/IncludeFunctions.cmake
@@ -1,3 +1,4 @@
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/CopyFilesToBinaryDir.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/LinkFilesToBinaryDir.cmake)
+include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/FindThirdPartyLibrary.cmake)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/InstallFilesWithDirectoryStructure.cmake)
