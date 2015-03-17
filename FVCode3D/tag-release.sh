73f8c0c34ade9de2ac5c011cd21a2c4876697cd2
Luca Pasquale <luca.pasquale@moxoff.com>
2015-03-17 15:58:04 +0100

    tag-release.sh now creates the tag after updating the RELEASE file

--- FVCode3D/tag-release.sh
+++ FVCode3D/tag-release.sh
@@ -1,13 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 
 DATE=`date +%x`
 DATE_TAG=`date +%Y%m%d`
 TAG_NAME="Release_Kafres-Upscaling_${DATE_TAG}"
 RELEASE_FILE="RELEASE"
 
+echo ${TAG_NAME} > ${RELEASE_FILE}
+git commit -m "Updated RELEASE file for version ${TAG_NAME}" ${RELEASE_FILE}
+echo "Updated RELEASE file and commited the change"
 git tag -a "${TAG_NAME}" -m "Version released to ENI on ${DATE}"
-echo $TAG_NAME > $RELEASE_FILE
-echo -e "New tag ${TAG_NAME} created.\n\
-Remember to push the tag and the ${RELEASE_FILE} file.\n\
-Type the following code to push the tag:\n\
-git push origin ${TAG_NAME}"
+echo "New tag ${TAG_NAME} created."
