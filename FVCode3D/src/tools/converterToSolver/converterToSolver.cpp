6105f1c737e37fb6a19ca48d31bbbeb031a64dcd
Stefano Zonca <zstefano123@gmail.com>
2015-04-25 13:47:19 +0200

    Updated converterToSolver to handle the new file formats.

--- FVCode3D/src/tools/converterToSolver/converterToSolver.cpp
+++ FVCode3D/src/tools/converterToSolver/converterToSolver.cpp
@@ -18,66 +18,72 @@ using namespace FVCode3D;
 
 int main(int argc, char * argv[])
 {
-	GetPot command_line(argc,argv);
-	const std::string dataFileName = command_line.follow("data.txt", 2, "-f", "--file");
+    GetPot command_line(argc,argv);
+    const std::string dataFileName = command_line.follow("data.txt", 2, "-f", "--file");
 
-	std::cout << "Read Data..." << std::flush;
-	Data data(dataFileName);
-	data.fractureOn(true);
-	data.verbose(true);
-	std::cout << " done." << std::endl;
+    std::cout << "Read Data..." << std::flush;
+    Data data(dataFileName);
+    data.fractureOn(true);
+    data.verbose(true);
+    std::cout << " done." << std::endl;
 
-	std::cout << std::endl;
-	data.showMe();
-	std::cout << std::endl;
+    std::cout << std::endl;
+    data.showMe();
+    std::cout << std::endl;
 
-	std::cout << "Define Mesh and Properties..." << std::flush;
-	Mesh3D mesh;
-	PropertiesMap propMap(data.getMobility());
-	std::cout << " done." << std::endl;
+    std::cout << "Define Mesh and Properties..." << std::flush;
+    Mesh3D mesh;
+    PropertiesMap propMap(data.getMobility());
+    std::cout << " done." << std::endl;
 
-	Importer * importer = 0;
+    Importer * importer = 0;
 
-	std::cout << "Create Importer..." << std::flush;
-	if(data.getMeshType() == Data::MeshFormatType::forSolver)
-		importer = new ImporterForSolver(data.getMeshDir() + data.getMeshFile(), mesh, propMap);
-	if(data.getMeshType() == Data::MeshFormatType::Medit)
-		importer = new ImporterMedit(data.getMeshDir() + data.getMeshFile(), mesh, propMap);
-	std::cout << " done." << std::endl;
+    std::cout << "Create Importer..." << std::flush;
+    if(data.getMeshType() == Data::MeshFormatType::forSolver)
+        importer = new ImporterForSolver(data.getMeshDir() + data.getMeshFile(), mesh, propMap);
+    else if(data.getMeshType() == Data::MeshFormatType::Medit)
+        importer = new ImporterMedit(data.getMeshDir() + data.getMeshFile(), mesh, propMap);
+    else if(data.getMeshType() == Data::MeshFormatType::TetGen)
+        importer = new ImporterTetGen(data.getMeshDir() + data.getMeshFile(), mesh, propMap);
+    else if(data.getMeshType() == Data::MeshFormatType::OpenFOAM)
+        importer = new ImporterOpenFOAM(data.getMeshDir() + data.getMeshFile(), mesh, propMap);
+    std::cout << " done." << std::endl;
 
-	std::cout << "Import grid file..." << std::flush;
-	importer->import(data.fractureOn());
-	std::cout << " done." << std::endl << std::endl;
+    std::cout << "Import grid file..." << std::flush;
+    importer->import(data.fractureOn());
+    std::cout << " done." << std::endl << std::endl;
 
-	std::cout << "Compute separated cells..." << std::flush;
-	mesh.updateFacetsWithCells();
-	std::cout << " done." << std::endl;
+    std::cout << "Compute separated cells..." << std::flush;
+    mesh.updateFacetsWithCells();
+    std::cout << " done." << std::endl;
 
-	std::cout << "Compute neighboring cells..." << std::flush;
-	mesh.updateCellsWithNeighbors();
-	std::cout << " done." << std::endl << std::endl;
+    std::cout << "Compute neighboring cells..." << std::flush;
+    mesh.updateCellsWithNeighbors();
+    std::cout << " done." << std::endl << std::endl;
 
-	std::cout << "Set labels on boundary & add Fractures..." << std::flush;
-	importer->addBCAndFractures(data.getTheta());
-	std::cout << " done." << std::endl << std::endl;
+    std::cout << "Set labels on boundary & add Fractures..." << std::flush;
+    importer->addBCAndFractures(data.getTheta());
+    std::cout << " done." << std::endl << std::endl;
 
-	std::cout << "Compute facet ids of the fractures..." << std::flush;
-	mesh.updateFacetsWithFractures();
-	std::cout << " done." << std::endl << std::endl;
+    std::cout << "Compute facet ids of the fractures..." << std::flush;
+    mesh.updateFacetsWithFractures();
+    std::cout << " done." << std::endl << std::endl;
 
     std::shared_ptr<PermeabilityBase> matrixPerm( new PermeabilityScalar );
     std::shared_ptr<PermeabilityBase> fracturesPerm( new PermeabilityScalar );
-	if(data.getMeshType() == Data::MeshFormatType::Medit)
-	{
-	    matrixPerm->setPermeability(data.getMatrixPermeability(), 0);
-	    fracturesPerm->setPermeability(data.getFracturePermeability(), 0);
-		propMap.setPropertiesOnMatrix(mesh, data.getMatrixPorosity(), matrixPerm);
-		propMap.setPropertiesOnFractures(mesh, data.getFractureAperture(), data.getFracturePorosity(), fracturesPerm);
-	}
+    if(data.getMeshType() == Data::MeshFormatType::Medit ||
+       data.getMeshType() == Data::MeshFormatType::TetGen ||
+       data.getMeshType() == Data::MeshFormatType::OpenFOAM)
+    {
+        matrixPerm->setPermeability(data.getMatrixPermeability(), 0);
+        fracturesPerm->setPermeability(data.getFracturePermeability(), 0);
+        propMap.setPropertiesOnMatrix(mesh, data.getMatrixPorosity(), matrixPerm);
+        propMap.setPropertiesOnFractures(mesh, data.getFractureAperture(), data.getFracturePorosity(), fracturesPerm);
+    }
 
-	std::cout << "Save as solver format..." << std::flush;
-	saveAsSolverFormat(data.getOutputDir() + data.getOutputFile() + ".fvg", mesh, propMap);
-	std::cout << " done." << std::endl << std::endl;
+    std::cout << "Save as solver format..." << std::flush;
+    saveAsSolverFormat(data.getOutputDir() + data.getOutputFile() + ".fvg", mesh, propMap);
+    std::cout << " done." << std::endl << std::endl;
 
-	return 0;
+    return 0;
 }
