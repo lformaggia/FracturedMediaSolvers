695b521474c1acf4c00eedcfa8b6834c297791ed
formaggia <luca.formaggia@gmail.com>
2018-10-31 15:48:51 +0100

    pulito un po

--- FVCode3D/codeblocks_project.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# To generate a codeblock project
-# Launch it from this directory. Codeblck directory will be ./codeblock/
-mkdir codeblock
-cd codeblock
-cmake .. -G"CodeBlocks - Unix Makefiles"
