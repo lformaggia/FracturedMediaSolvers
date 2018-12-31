9a26279645cabfa1ab97db2af0c8d0085fc3fc55
formaggia <luca.formaggia@gmail.com>
2018-12-31 17:11:06 +0100

    Modified for ubuntu 18.04 which does not replicate libblas and liblapack in /usr/lib

--- FVCode3D/install-eclipse-luca.sh
+++ FVCode3D/install-eclipse-luca.sh
@@ -4,9 +4,9 @@ HERE=`pwd`
 TARGET_DIR=${HERE}/../eclipse-project
 # Varius packages location, from module environment
 CMAKE_DIR=/usr/
-BLAS_LIB=/usr/lib/
-EIGEN_DIR=/usr/local/include/eigen3b
-LAPACK_DIR=/usr/lib/
+BLAS_LIB=/usr/lib/x86_64-linux-gnu
+EIGEN_DIR=/usr/local/include/eigen3
+LAPACK_DIR=/usr/lib/x86_64-linux-gnu
 SUITESPARSE_DIR=/usr/lib/x86_64-linux-gnu
 /bin/bash ./install-eclipse-project.sh --skip-packages-installation --prefix=${TARGET_DIR} \
 --with-cmake=${CMAKE_DIR} --with-blas=${BLAS_LIB} --with-lapack=${LAPACK_DIR} \
